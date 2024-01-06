2823e6a50f77aa5d182ca64b7554c08a58b6c2eb
Ahmed <117286352+AhmedProjet@users.noreply.github.com>
2024-01-06 14:42:44 +0100

    Fichier qui automatise le process

--- automatisation.sh
+++ automatisation.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-POSTGRES_USER="postgres"
-POSTGRES_PASSWORD="postgres"
+if [ "$#" -ne 2 ]; then
+    echo "Utilisation : $0 <nom_utilisateur_postgres> <mot_de_passe_postgres>"
+    exit 1
+fi
+
+POSTGRES_USER="$1"
+POSTGRES_PASSWORD="$2"
 ENV_FILE="$HOME/Recommandation_Project/.env"
 
 docker volume create Data
@@ -13,11 +18,11 @@ if [ ! -f "$ENV_FILE" ]; then
     echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> "$ENV_FILE"
 fi
 
-sed -i 's/username = "xxx"/username = "postgres"/g' "$HOME/Recommandation_Project/application/.streamlit/secrets.toml"
-sed -i 's/password = "xxx"/password = "postgres"/g' "$HOME/Recommandation_Project/application/.streamlit/secrets.toml"
+sed -i "s/username = \"xxx\"/username = \"$POSTGRES_USER\"/g" "$HOME/Recommandation_Project/application/.streamlit/secrets.toml"
+sed -i "s/password = \"xxx\"/password = \"$POSTGRES_PASSWORD\"/g" "$HOME/Recommandation_Project/application/.streamlit/secrets.toml"
 
 cd ~/Recommandation_Project/
 docker-compose up -d
 
-docker exec -it recommandation_project_postgres_1 psql -U postgres -d recommandations -a -f /docker-entrypoint-initdb.d/init-script.sql > /dev/null 2>&1
+docker exec -it recommandation_project_postgres_1 psql -U "$POSTGRES_USER" -d recommandations -a -f /docker-entrypoint-initdb.d/init-script.sql > /dev/null 2>&1
 
